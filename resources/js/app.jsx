c3eb0f74190d80f80e2242f61a2ec2e0afc35dc3
saif-dev154 <saif.dev154@gmail.com>
2026-05-14 14:55:33 +0500

    403 error solve

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,9 +1,16 @@
 import '../css/app.css';
 import './bootstrap';
 
-import { createInertiaApp } from '@inertiajs/react';
+import { createInertiaApp, router } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
+
+router.on('invalid', (e) => {
+    if (e.detail.response.status === 419 || e.detail.response.status === 403) {
+        e.preventDefault();
+        window.location.reload();
+    }
+});
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,10 @@
 import axios from 'axios';
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true;
+window.axios.defaults.withXSRFToken = true;
+
+const token = document.head.querySelector('meta[name="csrf-token"]');
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+}
